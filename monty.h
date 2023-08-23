c1bbe75eda7fb593cb1fb8b7a92a475a39114cb7
Eric Onasis <ericonasis@gmail.com>
2023-08-24 00:05:40 +0300

    Add functions prototypes

--- monty.h
+++ monty.h
@@ -54,5 +54,15 @@ int is_alphabetical(int c);
 void print_string(stack_t **stack,
 		__attribute__((unused)) unsigned int line_number);
 void print_char(stack_t **stack, unsigned int line_number);
+int is_number(const char *str);
+char *parse_line(char *line, stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void nop(__attribute__((unused)) stack_t **stack,
+		__attribute__((unused)) unsigned int line_number);
+void rotate_left(stack_t **stack,
+		__attribute__((unused)) unsigned int line_number);
+void rotate_right(stack_t **stack,
+		__attribute__((unused)) unsigned int line_number);
 
 #endif
