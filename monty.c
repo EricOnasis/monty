e1b4e5eaee28cbd2f8b4faa499a46bc216185837
Eric Onasis <ericonasis@gmail.com>
2023-08-23 23:24:31 +0300

    Add main monty program

--- monty.c
+++ monty.c
@@ -1,2 +1,38 @@
 #include "monty.h"
 
+/**
+ * main - main body for the monty program
+ * @argc: int num of arguments
+ * @argv: opcode file
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+	fprintf(stderr, "USAGE: monty file\n");
+	exit(EXIT_FAILURE);
+	}
+
+	read_file(argv[1], &stack);
+	free_stack(stack);
+	return (EXIT_SUCCESS);
+}
+
+/**
+ * free_stack - free a list
+ * @stack: pointer to first node
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *tmp;
+
+	while (stack)
+	{
+	tmp = stack;
+	stack = stack->next;
+	free(tmp);
+	}
+}
