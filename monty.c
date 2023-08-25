afdf064e60cd7460ac77eac1d8b2ddf5871e3bfc
Eric Onasis <ericonasis@gmail.com>
2023-08-25 03:41:31 +0300

    Fix Betty

--- monty.c
+++ monty.c
@@ -84,38 +84,39 @@ instruction_func get_opcode_function(char *str)
  */
 void read_file(char *filename, stack_t **stack)
 {
-    char *line;
-    size_t len = 0;
-    int line_number = 1;
-    instruction_func instruction;
-    (void)len;
+	char *line;
+	size_t len = 0;
+	int line_number = 1;
+	instruction_func instruction;
+	(void)len;
 
-    var_global.file = fopen(filename, "r");
-    if (var_global.file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
+	var_global.file = fopen(filename, "r");
+	if (var_global.file == NULL)
+	{
+	fprintf(stderr, "Error: Can't open file %s\n", filename);
+	exit(EXIT_FAILURE);
+	}
 
-    while (fgets(var_global.buffer, sizeof(var_global.buffer), var_global.file) != NULL)
-    {
-        line = var_global.buffer;
-        line[strcspn(line, "\n")] = '\0';
-        line = parse_line(var_global.buffer, stack, line_number);
-        if (line == NULL || line[0] == '#')
-        {
-            line_number++;
-            continue;
-        }
-        instruction = get_opcode_function(line);
-        if (instruction == NULL)
-        {
-            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, line);
-            exit(EXIT_FAILURE);
-        }
-        instruction(stack, line_number);
-        line_number++;
-    }
+	while (fgets(var_global.buffer, sizeof(var_global.buffer),
+				var_global.file) != NULL)
+	{
+	line = var_global.buffer;
+	line[strcspn(line, "\n")] = '\0';
+	line = parse_line(var_global.buffer, stack, line_number);
+	if (line == NULL || line[0] == '#')
+	{
+	line_number++;
+	continue;
+	}
+	instruction = get_opcode_function(line);
+	if (instruction == NULL)
+	{
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, line);
+	exit(EXIT_FAILURE);
+	}
+	instruction(stack, line_number);
+	line_number++;
+	}
 
-    fclose(var_global.file);
+	fclose(var_global.file);
 }
